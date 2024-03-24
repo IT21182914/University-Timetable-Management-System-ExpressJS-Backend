7784cfdd3fd5a57ffc86047e7d5c9ed5ba213df2
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-03-24 21:29:57 +0530

    Update

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://dilanshanuka999:1234@timetable.kgjq18u.mongodb.net/timetable?retryWrites=true&w=majority&appName=timetable",
+  mongoURI: "",
   jwtSecret: "myKey",
 };
